da6cc4602b868b3debc78130f6e178a2fabb1b3a
Bruno Estevinho <bruno.m.estevinho@gmail.com>
2020-12-03 16:40:35 +0000

    todos filtered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setfilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos,status])
+
+  const filterHandler = () => {
+    switch (status){
+      case 'completed':
+        setfilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setfilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default: 
+        setfilteredTodos(todos)
+        break;
+    }
+  }
 
   return (
     <div className="App">
@@ -18,8 +39,9 @@ function App() {
         todos={todos} 
         setTodos={setTodos} 
         setInputText={setInputText} 
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
