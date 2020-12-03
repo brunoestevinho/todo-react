dacf92d3e69d641cede9e1ea04ca859286763337
Bruno Estevinho <bruno.m.estevinho@gmail.com>
2020-12-03 17:04:23 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Bruno's Todo List</h1>
+        <h1>Take note !</h1>
       </header>
       <Form 
         inputText={inputText} 
