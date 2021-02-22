058f3f5ac5bf0ee46363b23fc9cbd85c1575621d
Bruno Estevinho <bruno.m.estevinho@gmail.com>
2021-02-22 20:34:22 +0000

    app name corrected

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { Navbar } from 'react-bootstrap';
+import React from "react";
+import { Navbar } from "react-bootstrap";
 
 //Auth0
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./LogoutButton";
 
-
 const Header = () => {
-  
   const { user, isAuthenticated } = useAuth0();
-  return(
+  return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>Take Note!</Navbar.Brand>
+        <Navbar.Brand>2Do!</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-        <Navbar.Brand>
-        {isAuthenticated && (<img className="avatar" src={user.picture} alt="Avatar"/>)}
-        </Navbar.Brand>    
+          <Navbar.Brand>
+            {isAuthenticated && (
+              <img className="avatar" src={user.picture} alt="Avatar" />
+            )}
+          </Navbar.Brand>
         </Navbar.Collapse>
         <LogoutButton />
       </Navbar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
